df955cc956840045b7d74bb9267817d8cb5d9547
Kassandra Poon <kassandra.poon@zgm.ca>
2019-03-06 13:12:36 -0700

    Darker tags and tags SE

--- src/themes/light-custom-blue.js
+++ src/themes/light-custom-blue.js
@@ -10,8 +10,8 @@ const common = {
 }         
 
 const syntax = {
-  tag: $`1cb5bb`,                 // #55B4D4 to #09a3d7 to #1cb5bb
-  tagSE: $`a291ec`,               // #a291ec
+  tag: $`00b4bb`,                 // #55B4D4 to #09a3d7 to #00b4bb
+  tagSE: $`9b88eb`,               // #a291ec to #9b88eb
   tagAttr: $`ec8c38`,             // #ec8c38
   func: $`09a3d7`,                // #F29718 to #be7acf or to #0982d7
   entity: $`1cb5bb`,              // #399EE6 to #55b4d4 ??? to #1cb5bb
